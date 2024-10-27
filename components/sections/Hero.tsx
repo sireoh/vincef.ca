ba13388ec715e31753b3a9dad69ee4a7c370230b
Vincent <eohndk@gmail.com>
2024-10-27 15:00:55 -0700

    fixed dependencies

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/no-unknown-property */
 "use client"
 
-import { FC, JSX, MouseEventHandler, useRef } from "react"
+import { FC, MouseEventHandler, useRef } from "react"
 import { Particles } from "../atoms/Particle"
 import * as utils from "./utils/utils"
 import { Data } from "./interfaces/Data"
 import import_data from "./data/data.json"
 const data: Data = import_data as unknown as Data;
+import Image from "next/image"
 
 export const Hero: FC = () => {
   const mouse = useRef<[number, number]>([0, 0])
@@ -21,7 +22,7 @@ export const Hero: FC = () => {
       <div className="mx-auto flex max-w-7xl flex-col items-center justify-evenly gap-16 px-4 md:px-16 lg:mt-32 lg:flex-row lg:items-start">
         <div className="mt-40 flex flex-col gap-16 lg:m-0 lg:w-1/2">
           <div className="gif flex justify-center">
-            <img width="300" height="259" alt="RaccoonSpaceGif" src="/media/space.webp"/>
+            <Image width="300" height="259" alt="RaccoonSpaceGif" src="/media/space.webp"/>
           </div>
           <div>
             <h2 className="text-base-content">Hello there,</h2>
