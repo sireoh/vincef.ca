e38f505d9e7db94cbae6d7bd916fcb8bf1751445
Vincent <eohndk@gmail.com>
2024-10-27 15:54:30 -0700

    kevin

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://kevinpek.com/sitemap.xml",
+    sitemap: "https://vincef.xyz/sitemap.xml",
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://kevinpek.com",
+      url: "https://vincef.xyz",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
